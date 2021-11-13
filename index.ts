70a40b3e5c3f7688ed72ec5acebdec350021bc1c
Bjornir90 <celestincollin@gmail.com>
2021-11-13 11:55:15 +0100

    Use tslint, and start app in development

--- index.ts
+++ index.ts
@@ -1,8 +1,21 @@
-// install express with `npm install express` 
-const express = require('express')
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+        // tslint:disable-next-line:no-console
+console.log("Environnement : "+process.env.NODE_ENV);
+
+if(process.env.NODE_ENV === "development"){
+    app.listen(process.env.PORT, () => {
+        // tslint:disable-next-line:no-console
+        console.log("API server started on "+process.env.PORT)
+    });
+}
 
 // export 'app'
 module.exports = app
